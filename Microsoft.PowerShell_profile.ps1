75c5c36694fb4b0656e2423680328986dd960468
Emil Backlund <emil.backl@gmail.com>
2015-09-08 09:54:44 +0200

    Initial commit of setup files.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+import-module icore.dev
+new-item alias:handle -value 'H:\My Dev Tools\Handle\Handle.exe'
+new-item alias:icore-admin -value F:\ETWork\Bin\Admin.exe
+new-item alias:icore-runcomp -value F:\ETWork\Bin\RunCompTool.exe
+new-item alias:icore-wf -value F:\ETWork\Bin\WorkflowDesigner.exe
+new-item alias:icore-dev -value F:\ETWork\Bin\Editor.exe
+#new-item alias:opencover -value C:\Users\bacemi\AppData\Local\Apps\OpenCover\OpenCover.Console.exe
+new-item alias:mpv -value 'F:\Development\NModel\bin\mpv.exe'
+new-item alias:otg -value 'F:\Development\NModel\bin\otg.exe'
+new-item alias:mp2dot -value 'F:\Development\NModel\bin\mp2dot.exe'
+new-item alias:ct -value 'F:\Development\NModel\bin\ct.exe'
+new-item alias:gacutil -value 'C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools\gacutil.exe'
+new-item alias:installutil -value 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe'
+
---
+++ Sublime_Settings/README.txt
@@ -0,0 +1,2 @@
+Files in this folder (excluding this file) should be pasted to %appdata%\Sublime Text 3\Packages\User. 
+Next time sublime starts packages will be downloaded
