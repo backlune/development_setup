2dd3ceb1dbcbdbaf3e8761e2bec7611c40388dba
Emil Backlund <emil.backl@gmail.com>
2016-10-02 20:16:20 +0200

    application setup with choclatey

---
+++ applicationsetup.ps1
@@ -0,0 +1,23 @@
+﻿#Set-ExecutionPolicy RemoteSigned
+
+#iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+#choco install evernote
+
+choco install cmder
+
+choco install visualstudiocode
+
+choco install 7zip.install
+
+choco install ccleaner
+
+choco install keepass.install
+
+choco install teamviewer
+
+choco install sublimetext3
+
+choco install spotify
+
+choco install adobereader
