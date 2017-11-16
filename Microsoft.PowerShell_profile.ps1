b16182f746bbde2f724fcd380352876f865cd050
Emil Backlund <bacemi@icore.se>
2017-11-16 10:41:53 +0100

    Code rush export and powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,18 @@
 import-module icore.dev
+Import-VSVars 2017
+$env:Platform=""
+
+# Install-Module z -AllowClobber
+Import-Module z
+#Install-Module posh-git -AllowClobber
+Import-Module posh-git
+
 new-item alias:handle -value 'H:\My Dev Tools\Handle\Handle.exe'
 new-item alias:icore-admin -value F:\ETWork\Bin\Admin.exe
 new-item alias:icore-runcomp -value F:\ETWork\Bin\RunCompTool.exe
 new-item alias:icore-wf -value F:\ETWork\Bin\WorkflowDesigner.exe
 new-item alias:icore-dev -value F:\ETWork\Bin\Editor.exe
+new-item alias:icore-api -value F:\ETWork\Bin\WebAPIEditor.exe
 new-item alias:icore-sin -value F:\ETWork\Bin\Tests\SInTEST.exe
 #new-item alias:opencover -value C:\Users\bacemi\AppData\Local\Apps\OpenCover\OpenCover.Console.exe
 new-item alias:mpv -value 'F:\Development\NModel\bin\mpv.exe'
@@ -14,10 +23,21 @@ new-item alias:gacutil -value 'C:\Program Files (x86)\Microsoft SDKs\Windows\v10
 new-item alias:installutil -value 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe'
 
 function Start-iCoreServer { Start-Service (Get-Service "iCoreMgmtSvc") }
-New-Alias -Name icore-start -Value Start-iCoreServer
-
 function Stop-iCoreServer { Stop-Service (Get-Service "iCoreMgmtSvc") }
-New-Alias -Name icore-stop -Value Stop-iCoreServer
+
+function Start-SAS{ Start-Service (Get-Service "iCoreSASvc") }
+function Stop-SAS { Stop-Service (Get-Service "iCoreSASvc") }
+
+function Start-iCore { 
+	Start-Service (Get-Service "iCoreMgmtSvc") 
+	Start-Service (Get-Service "iCoreSASvc")
+}
+
+function Stop-iCore 
+{ 
+	Stop-Service (Get-Service "iCoreSASvc") 
+	Stop-Service (Get-Service "iCoreMgmtSvc")
+}
 
 
 function Clear-iCoreReg { 
@@ -32,4 +52,13 @@ function Fix-iCoreJuncation{
 
 	Update-iCoreJunctions $Branch -Force
 }
-New-Alias -Name icore-fixjuncation-Value Fix-iCoreJuncation
+New-Alias -Name icore-fixjuncation -Value Fix-iCoreJuncation
+
+function ServicingToMain { Switch-iCoreBranch -CurrentBranchName Servicing.4.00 -TargetBranchName Main -Force }
+function ServicingToHotfix { Switch-iCoreBranch -CurrentBranchName Servicing.4.00 -TargetBranchName Hotfix.4.00.0 -Force }
+function MainToServicing { Switch-iCoreBranch -CurrentBranchName Main -TargetBranchName Servicing.4.00 -Force }
+function MainToHotfix { Switch-iCoreBranch -CurrentBranchName Main -TargetBranchName Hotfix.4.00.0 -Force }
+function HotfixToServicing { Switch-iCoreBranch -CurrentBranchName Hotfix.4.00.0 -TargetBranchName Servicing.4.00 -Force }
+function HotfixToMain { Switch-iCoreBranch -CurrentBranchName Hotfix.4.00.0 -TargetBranchName Main -Force }
+
+function Which-iCoreBranch { Get-Item 'F:\ETWork\Bin\Project Templates\' | Select-Object -ExpandProperty Target }
