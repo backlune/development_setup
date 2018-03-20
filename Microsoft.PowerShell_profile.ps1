8f5c5d39df4936eae6f8386c47f771ebebda1e3e
Emil Backlund <bacemi@icore.se>
2018-03-20 08:39:05 +0100

    Updated setup with latest psprofile, subilme, coderush, visual studio and cmder settings

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,3 @@
-import-module icore.dev
 Import-VSVars 2017
 $env:Platform=""
 
@@ -7,20 +6,34 @@ Import-Module z
 #Install-Module posh-git -AllowClobber
 Import-Module posh-git
 
-new-item alias:handle -value 'H:\My Dev Tools\Handle\Handle.exe'
+
+### Setup Toggle Api key ###
+$env:TogglApiKey=""
+
+
+### Development Tools ###
+new-item alias:handle -value 'D:\My Dev Tools\Handle\Handle.exe'
+new-item alias:gacutil -value 'C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.1 Tools\gacutil.exe'
+new-item alias:installutil -value 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe'
+new-item alias:asminfo -value 'C:\tools\AsmInfo.exe'
+
+
+### Testing tools ###
+#new-item alias:opencover -value C:\Users\bacemi\AppData\Local\Apps\OpenCover\OpenCover.Console.exe
+new-item alias:mpv -value 'F:\Development\NModel\bin\mpv.exe'
+new-item alias:otg -value 'F:\Development\NModel\bin\otg.exe'
+new-item alias:mp2dot -value 'F:\Development\NModel\bin\mp2dot.exe'
+new-item alias:ct -value 'F:\Development\NModel\bin\ct.exe'
+
+
+### iCIS Development ###
+import-module icore.dev
 new-item alias:icore-admin -value F:\ETWork\Bin\Admin.exe
 new-item alias:icore-runcomp -value F:\ETWork\Bin\RunCompTool.exe
 new-item alias:icore-wf -value F:\ETWork\Bin\WorkflowDesigner.exe
 new-item alias:icore-dev -value F:\ETWork\Bin\Editor.exe
 new-item alias:icore-api -value F:\ETWork\Bin\WebAPIEditor.exe
 new-item alias:icore-sin -value F:\ETWork\Bin\Tests\SInTEST.exe
-#new-item alias:opencover -value C:\Users\bacemi\AppData\Local\Apps\OpenCover\OpenCover.Console.exe
-new-item alias:mpv -value 'F:\Development\NModel\bin\mpv.exe'
-new-item alias:otg -value 'F:\Development\NModel\bin\otg.exe'
-new-item alias:mp2dot -value 'F:\Development\NModel\bin\mp2dot.exe'
-new-item alias:ct -value 'F:\Development\NModel\bin\ct.exe'
-new-item alias:gacutil -value 'C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\gacutil.exe'
-new-item alias:installutil -value 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe'
 
 function Start-iCoreServer { Start-Service (Get-Service "iCoreMgmtSvc") }
 function Stop-iCoreServer { Stop-Service (Get-Service "iCoreMgmtSvc") }
